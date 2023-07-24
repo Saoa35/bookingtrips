a5ad76cfb2240778f6902dce5188c42b75c174b2
Saoa35 <saoamah@gmail.com>
2023-07-24 16:35:49 +0400

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route />
+      </Routes>
     </>
   );
 }
