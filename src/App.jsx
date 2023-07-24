b2d4b38ff3164947d18e3923d4e90dfbea56ed9d
Saoa35 <saoamah@gmail.com>
2023-07-24 18:51:24 +0400

    + created bookings component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import LogIn from "./pages/LogIn";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<Registration />} />
         <Route path="/sign-in" element={<LogIn />} />
-        {/* <Route path="/bookings" element={<LogIn />} /> */}
+        <Route path="/bookings" element={<Bookings />} />
         {/* <Route path="/trip/:tripId " element={<LogIn />} /> */}
         <Route path="*" element={<Home />} />
       </Routes>
