7d194a5f6151a67bb0b8f12928e7aa9dc47d7348
Saoa35 <saoamah@gmail.com>
2023-07-26 10:23:44 +0400

    + BookingCard

--- src/components/cards/BookingCard.jsx
+++ src/components/cards/BookingCard.jsx
@@ -1,3 +1,25 @@
-export const BookingCard = () => {
-  return;
+export const BookingCard = ({ title, guests, createdAt, totalPrice }) => {
+  return (
+    <li data-test-id="booking" className="booking">
+      <h3 data-test-id="booking-title" className="booking__title">
+        {title}
+      </h3>
+      <span data-test-id="booking-guests" className="booking__guests">
+        {guests} guests
+      </span>
+      <span data-test-id="booking-date" className="booking__date">
+        {createdAt}
+      </span>
+      <span data-test-id="booking-total" className="booking__total">
+        {totalPrice} $
+      </span>
+      <button
+        data-test-id="booking-cancel"
+        className="booking__cancel"
+        title="Cancel booking"
+      >
+        <span className="visually-hidden">Cancel booking</span>×
+      </button>
+    </li>
+  );
 };
